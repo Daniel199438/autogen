994fc1783459c685518b404eb71dc37d3ab2895c
Daniel Schneider <daniel.schneider@oneworx.de>
2023-12-11 11:58:08 +0100

    add port 8099 to allowed origins and change cmd command to start app

--- samples/apps/autogen-assistant/autogenra/web/app.py
+++ samples/apps/autogen-assistant/autogenra/web/app.py
@@ -44,6 +44,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "http://127.0.0.1:8099",
+        "http://localhost:8099",
         "http://localhost:8001",
         "http://localhost:8081",
     ],
